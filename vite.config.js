42f00a7ecfcf1d4b3488ab03286b79377cc6c0ba
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2026-03-05 19:46:19 -0800

    fixed the base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ⚡ Replace "your-repo-name" with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/RashidaLittleChampsDayCare",  // 👈 very important for GitHub Pages
+  base: "/RashidaLittleChampsDayCare/",
   build: {
     outDir: "dist",
   },
